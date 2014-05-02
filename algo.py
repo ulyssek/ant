b79e82f162229b3b0fdec91e9b5db3db44067c2b
hulyce <ulysseklatzmann@gmail.com>
2014-05-02 13:48:35 +0200

    stuff

--- algo.py
+++ algo.py
@@ -1,32 +1,83 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
 from graph import Graph
 from random import uniform
 
 class Algo():
 
-  DEFAULT_WEIGTH = 0.5
+  ############################################################################################################
+  ## CLASS VARS
+  DEFAULT_WEIGHT  = 0.1
+  ITERATIONS      = 10
 
-  def __init__(self):
+  ############################################################################################################
+  ## INIT FUNCTIONS
+
+  def __init__(self, mode="classic"):
+    self.mode = mode
     self.initial_graph = Graph()
     self.current_graph = self.initial_graph
     self.ant_positions = range(self.initial_graph.get_size())
-    self.initial_graph.print_graph()
 
-  def run(self):
-    for ant in self.ant_positions:
-      self.__next_position__(ant)
+  ############################################################################################################
+  ## RUNNING FUNCTIONS
 
+  def launch(self):
+    for i in xrange(self.ITERATIONS):
+      self._run()
+      self.print_current()
+
+  def _run(self):
+    """
+    lance une itération de l'algorithme
+    """
+    running_edges = []
+    for i in xrange(len(self.ant_positions)):
+      next_position = self.__next_position__(self.ant_positions[i])
+      running_edges.append((self.ant_positions[i], next_position))
+      self.ant_positions[i] = next_position 
+    self.__update__(running_edges)
+
+  ############################################################################################################
+  ## UPDATE FUNCTIONS
+  
+  def __update__(self, edges):
+    """
+    Update des poids du graphe
+    """
+    for i, j in edges:
+      self.current_graph.get_graph()[i][j] += self.DEFAULT_WEIGHT*2
+      self.current_graph.get_graph()[j][i] += self.DEFAULT_WEIGHT*2
+    self.current_graph.minus(self.DEFAULT_WEIGHT) 
+    
+  ############################################################################################################
+  ## PRINTING FUNCTIONS 
+
+  def print_current(self):
+    print "ant's positions :"
+    print self.ant_positions
+    print "Current graph : "
+    print self.current_graph
+
+  ############################################################################################################
+  ## OTHER FUNCTIONS
   def __next_position__(self, ant):
+    """
+    Calcul la prochaine position d'une fourmies données à partir du graphe courant
+    """
     graph_line = self.current_graph.get_graph()[ant]
     sum_weight = sum(graph_line)
+    if sum_weight == 0:
+      #Si toutes les voies sont coupées, la fourmies ne bouge pas
+      return ant
+    #Calcul du vecteur de probabilité
     probas = map(lambda x : float(x)/sum_weight, graph_line)
+    # Détermination de la position suivante
     random_number = uniform(0., 1.)
     count = 0
     for i in xrange(len(probas)):
       count += probas[i]
       if count >= random_number:
-        print probas
-        print random_number
-        print i
         return i
+    #sum(probas)==1, donc le script ne peut pas sortir de cette boucle autrement que pas un return.
--- graph.py
+++ graph.py
@@ -4,6 +4,9 @@ from random import randint
 
 class Graph():
 
+  ############################################################################################################
+  ## CLASS VARS
+
   DEFAULT_GRAPH = [
     [0,7,6,2,1],
     [7,0,5,3,3],
@@ -14,6 +17,9 @@ class Graph():
 
   DEFAULT_SIZE = 5
 
+  ############################################################################################################
+  ## INIT FUNCTIONS
+
   def __init__(self, mode="default"):
     if mode == "default":
       self._graph = self.DEFAULT_GRAPH
@@ -21,18 +27,6 @@ class Graph():
       self._graph = self._random_graph_()
     else:
       raise Exception("Not implemented yet")
-
-  ############################################################################################################
-  ## GET FUNCTIONS
-
-  def get_size(self):
-    return len(self._graph)
-
-  def get_graph(self):
-    return self._graph
-
-  ############################################################################################################
-  ## OTHER FUNCTIONS
 
   def _random_graph_(self):
     graph = []
@@ -45,5 +39,30 @@ class Graph():
           graph[i].append(randint(0,8))
     return graph
 
-  def print_graph(self):
-    print self._graph
+  ############################################################################################################
+  ## GET FUNCTIONS
+
+  def get_size(self):
+    return len(self._graph)
+
+  def get_graph(self):
+    return self._graph
+
+  ############################################################################################################
+  ## GRAPH ALTERATION FUNCTIONS
+
+  def minus(self, minus):
+    for i in xrange(self.get_size()):
+      for j in xrange(self.get_size()):
+        self._graph[i][j] = max(0, self._graph[i][j]-minus)
+
+  def normalize(self):
+    for i in xrange(self.get_size()):
+      sum_weight = sum(self._graph[i])
+      self._graph[i] = map(lambda x : float(x)/sum_weight, self._graph[i])
+
+  ############################################################################################################
+  ## PRINTING FUNCTIONS
+
+  def __str__(self):
+    return str(self._graph)
